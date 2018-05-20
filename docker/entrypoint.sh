246bbf126cd865252455b1ddb16b1b84fb09aaf2
Kilian <kilian@nexus-stats.com>
2018-05-20 15:03:44 +0200

    ci(watchdog): Add traffic control to reduce network usage.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd /app/warframe-items
 
+# Throttle connection. I just don't want this stuff to make my network lag.
+tc qdisc add dev eth0 handle 1: ingress
+tc filter add dev eth0 parent 1: protocol ip prio 50 u32 match ip src 0.0.0.0/0 police rate 1mbit burst 10k drop flowid :1
+tc qdisc add dev eth0 root tbf rate 1mbit latency 25ms burst 10k
+
 while : ; do
   # Run update function. This will only update files if necessary.
   npm run watchdog
