1d2d5a12183f3ab8dd28b69cdc7c75eff52d933c
Kilian <kilian@nexus-stats.com>
2018-05-20 11:35:56 +0200

    fix(docker): Add some logging

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,12 +11,16 @@ while : ; do
     gh_token=$(cat /run/secrets/warframe-items-gh-token)
     date=`date +%Y-%m-%d`
 
+    echo '* Found new items - Pushing...'
     git config --global user.email "apps@nexus-stats.com"
     git config --global user.name "nexus-ci"
     git add .
     git commit -m "fix: Add new items for $date."
     git push 'https://nexus-ci:'$gh_token'@github.com/nexus-devs/warframe-items' staging 2>/dev/null
     # ^ /dev/null so the token won't be exposed in logs
+    echo '* Changes have been pushed to git!\n'
+  else
+    echo '* No new items.\n'
   fi
 
   sleep 60
