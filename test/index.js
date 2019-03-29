c1b6d38c92083efc102a2b5931511446a505df46
Kaptard <kilian@nexus-stats.com>
2019-03-29 07:45:45 +0100

    fix: Resolve issues with v8's internal .filter function on Items Object

--- test/index.js
+++ test/index.js
@@ -1,9 +1,18 @@
 const assert = require('assert')
 
 describe('index.js', function () {
+  // Smoke test
   it('should contain items when initializing.', function () {
     const Items = require('../index.js')
     const items = new Items()
     assert(items.length)
+  })
+
+  // Custom filter override in index.js
+  it('should not return more or equal the number of objects after .filter(), when custom categories are specified.', function () {
+    const Items = require('../index.js')
+    const items = new Items({ category: ['Primary'] })
+    const primes = items.filter(i => i.name.includes('Prime'))
+    assert(primes.length < items.length)
   })
 })
