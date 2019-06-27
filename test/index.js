d7dd85e7ed758c3a32f1bea7d4f1f552e4f9c3df
Tobiah <tobiah@protonmail.com>
2019-06-27 11:21:59 -0500

    feat(parser): enemy data parsing (#89)
    * chore: remove unneeded npmignore line
    
    * fix: flip logic on outer category check

--- test/index.js
+++ test/index.js
@@ -8,6 +8,12 @@ describe('index.js', function () {
     assert(items.length)
   })
 
+  it('should ignore enemies when configured.', function () {
+    const Items = require('../index.js')
+    const items = new Items({ ignoreEnemies: true }).filter(i => i.category === 'Enemy')
+    assert(!items.length)
+  })
+
   // Custom filter override in index.js
   it('should not return more or equal the number of objects after .filter(), when custom categories are specified.', function () {
     const Items = require('../index.js')
