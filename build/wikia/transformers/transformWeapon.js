WFCD/warframe-items

commit 4744d40f10d3febb225d85d277ebf68942585709

fix(wikia): Add in missing chargeTime (#127)
This is required to properly compute damage over time of continuous beam
weapons (E.g. the Phage), and charge-trigger energy weapons (e.g.
Opticor).

build/wikia/transformers/transformWeapon.js
@@ -138,6 +138,7 @@ const transformWeapon = (oldWeapon, imageUrls) => {
         }
       })
     } else if (ChargeAttack && ChargeAttack.Damage) {
+      newWeapon.chargeTime = Number(Number(ChargeAttack.ChargeTime));
       damageTypes.forEach((damageType) => {
         newWeapon[damageType.toLowerCase()] = ChargeAttack.Damage[damageType] ? Number(ChargeAttack.Damage[damageType].toFixed(2).replace(/(\.[\d]+)0/, '$1')) : undefined
       })

build/parser.js
@@ -666,6 +666,7 @@ class Parser {
     const damageTypes = require('../config/damageTypes.json')
     item.ammo = wikiaItem.ammo
     item.channeling = wikiaItem.channeling
+    item.chargeTime = wikiaItem.chargeTime
     item.damage = wikiaItem.damage
     item.damageTypes = {}
     damageTypes.forEach(type => {

index.d.ts
@@ -128,6 +128,7 @@ declare module 'warframe-items' {
         masteryReq?: number;
         omegaAttenuation?: number;
         ammo?: number;
+        chargeTime?: number;
         damage?: number | string;
         damageTypes?: DamageTypes;
         flight?: number;

commit a9e2f7bb6b33937405117eb24abb7a4d2b8a8f93

fix(build): corrects build failure when running lint
Commit 4744d40f introduced a trailing semicolon that violates the build
checks performed during `npm run lint`.

build/wikia/transformers/transformWeapon.js
@@ -138,7 +138,7 @@ const transformWeapon = (oldWeapon, imageUrls) => {
         }
       })
     } else if (ChargeAttack && ChargeAttack.Damage) {
-      newWeapon.chargeTime = Number(Number(ChargeAttack.ChargeTime));
+      newWeapon.chargeTime = Number(Number(ChargeAttack.ChargeTime))
       damageTypes.forEach((damageType) => {
         newWeapon[damageType.toLowerCase()] = ChargeAttack.Damage[damageType] ? Number(ChargeAttack.Damage[damageType].toFixed(2).replace(/(\.[\d]+)0/, '$1')) : undefined
       })