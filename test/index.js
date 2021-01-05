84b02501e6eec0245e2c3f3924bef8305a52e6ee
Matt Voboril <tobiah@protonmail.com>
2021-01-05 11:27:14 -0600

    fix(drops): Better Drop names regex (#164)

--- test/index.js
+++ test/index.js
@@ -21,4 +21,47 @@ describe('index.js', function () {
     const primes = items.filter(i => i.name.includes('Prime'))
     assert(primes.length < items.length)
   })
+
+  describe('drops should not include variant drops', () => {
+    it('should not have drops for hikou', () => {
+      const Items = require('../index.js')
+      const items = new Items({ category: ['Secondary'] })
+      const hikouMatches = items.filter(i => i.name === 'Hikou')
+      assert(hikouMatches.length === 1)
+      const { drops } = hikouMatches[0]
+      assert(typeof drops === 'undefined')
+    })
+
+    it('should only have non-prime drops for Gorgon', () => {
+      const Items = require('../index.js')
+      const items = new Items({ category: ['Primary'] })
+      const matches = items.filter(i => i.name === 'Gorgon')
+      assert(matches.length === 1)
+      const { drops } = matches[0]
+      assert(typeof drops === 'undefined')
+
+      const bp = matches[0].components.filter(component => component.name === 'Blueprint')[0]
+      assert(typeof bp !== 'undefined')
+      assert(bp.drops.length !== 0)
+
+      const primeBpDrops = bp.drops.filter(n => n.type.includes('Prime'))
+      assert(primeBpDrops.length === 0)
+    })
+
+    it('should have variant drops when requested', () => {
+      const Items = require('../index.js')
+      const items = new Items({ category: ['Primary'] })
+      const matches = items.filter(i => i.name === 'Gorgon Wraith')
+      assert(matches.length === 1)
+      const { drops } = matches[0]
+      assert(typeof drops === 'undefined')
+
+      const bp = matches[0].components.filter(component => component.name === 'Blueprint')[0]
+      assert(typeof bp !== 'undefined')
+      assert(bp.drops.length !== 0)
+
+      const variantDrops = bp.drops.filter(n => n.type.includes('Wraith'))
+      assert(variantDrops.length === 1)
+    })
+  })
 })
