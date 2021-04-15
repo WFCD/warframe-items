9c945988d05a6bb93079115c1c79cf0b4ea12062
Tobiah <tobiah@pm.me>
2021-04-15 00:29:33 -0500

    fix(processing): dedupe

--- build/stringify.js
+++ build/stringify.js
@@ -1,3 +1,5 @@
+const dedupe = require('./dedupe.js')
+
 /**
  * Pretty print JSON as it should be.
  * https://gist.github.com/nrkn/c7a89bb7039182314415
@@ -7,6 +9,6 @@ const isArrayOfPrimitive = obj => Array.isArray(obj) && obj.every(isPrimitive)
 const format = arr => `^^^[ ${arr.map(val => JSON.stringify(val)).join(', ')} ]`
 const replacer = (key, value) => isArrayOfPrimitive(value) ? format(value) : value
 const expand = str => str.replace(/(?:"\^\^\^)(\[ .* \])(?:")/g, (match, a) => a.replace(/\\"/g, '"'))
-const stringify = obj => expand(JSON.stringify(obj, replacer, 2))
+const stringify = obj => expand(JSON.stringify(Array.isArray(obj) ? dedupe(obj) : obj, replacer, 2))
 
 module.exports = stringify
