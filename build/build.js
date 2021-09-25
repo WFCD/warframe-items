291b415d0fac746b19bdd93fcadf5a979c26315f
Tobiah <tobiah@pm.me>
2021-09-24 21:27:02 -0500

    chore: lint

--- build/build.js
+++ build/build.js
@@ -64,7 +64,7 @@ class Build {
           } else {
             result[item.productCategory] = [item]
           }
-          continue;
+          continue
         }
 
         if (result[item.category]) {
