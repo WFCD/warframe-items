6b2ca15245577d99c819015bb7d3cc2c8796e502
Tobiah <tobiah@pm.me>
2021-11-30 22:27:49 -0600

    chore: lint

--- build/build.js
+++ build/build.js
@@ -85,7 +85,7 @@ class Build {
    * Generate JSON file for each category and one for all combined.
    */
   saveJson (categories, i18n) {
-    let all = []
+    const all = []
     const sort = (a, b) => {
       if (!a.name) console.log(a)
       const res = a.name.localeCompare(b.name)
