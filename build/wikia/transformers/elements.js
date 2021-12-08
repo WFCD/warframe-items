60dfd9ce2c10aac56795614181d8b27556b34a73
Matt Voboril <tobiah@pm.me>
2021-12-08 14:12:45 -0600

    fix: update inconsistent polarities (#291)
    Co-authored-by: André <42449362+AyAyEm@users.noreply.github.com>

---
+++ build/wikia/transformers/elements.js
@@ -0,0 +1,13 @@
+module.exports = {
+  Electricity: 'electricity',
+  Corrosive: 'corrosive',
+  Toxin: 'toxin',
+  Heat: 'heat',
+  Blast: 'blast',
+  Radiation: 'radiation',
+  Cold: 'cold',
+  Viral: 'viral',
+  Magnetic: 'magnetic',
+  Gas: 'gas',
+  Void: 'void'
+}
