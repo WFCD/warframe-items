5dc796190a9a4962df95d55c158b8efcbf15be57
Tobiah <tobiah@pm.me>
2022-01-24 12:21:32 -0600

    fix: separate built prime items into separate check
    add rifles mods type clarification

--- build/parser.js
+++ build/parser.js
@@ -530,22 +530,7 @@ class Parser {
    * Limit items to tradable/untradable if specified.
    */
   addTradable (item) {
-    const tradableTypes = ['Upgrades', 'Arcane', 'Fish', 'Focus Lens', 'Relic', 'Rifle Mod',
-      'Secondary Mod', 'Shotgun Mod', 'Warframe Mod', 'Companion Mod', 'Archwing Mod', 'K-Drive Mod',
-      'Melee Mod', 'Arch-Melee Mod', 'Necramech Mod', 'Cut Gem', 'Captura', 'Primary Mod', 'Sentinel Mod', 'Kubrow Mod', 'Kavat Mod']
-    const untradableTypes = ['Skin', 'Medallion', 'Key', 'Extractor', 'Pets', 'Ship Decoration',
-      'Glyph', 'Sigil', 'Fur Color', 'Syandana', 'Fur Pattern', 'Color Palette', 'Node', 'Exalted Weapon', 'Warframe',
-      'Throwing', 'Shotgun', 'Rifle', 'Pistol', 'Melee', 'Sword And Shield']
-    const tradableRegex = /(Prime|Vandal|Wraith\w|\wWraith|Rakta|Synoid|Sancti|Vaykor|Telos|Secura|Ayatan|Prisma|DamagedMech)(?!Derelict)/i
-    const untradableRegex = /(Glyph|Mandachord|Greater.*Lens|Sugatra|\[|SentinelWeapons|Toroid|Bait|([A-Za-z]+ (Relic)))|Umbral|Sacrificial/i
-    const notFiltered = !untradableTypes.includes(item.type) &&
-      !item.name.match(untradableRegex) &&
-      !item.uniqueName.match(untradableRegex) &&
-      (item.productCategory ? !item.productCategory.match(/(SpecialItems)/) : true)
-    const tradableByType = !!(tradableTypes.includes(item.type) && notFiltered)
-    const tradableByName = !!((item.uniqueName.match(tradableRegex) || item.name.match(tradableRegex)) && notFiltered)
-    const tradableByProp = !!((item.isAugment) && notFiltered)
-    item.tradable = tradableByType || tradableByName || tradableByProp
+    item.tradable = require('./tradable')(item)
   }
 
   /**
