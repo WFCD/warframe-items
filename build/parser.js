23ef4abd1ad0348dc1d2084156a0183cdcb21974
Ornstein <HydroNav@protonmail.com>
2022-09-16 17:42:59 -0400

    feat: add `isPrime` flag (#371)

--- build/parser.js
+++ build/parser.js
@@ -182,6 +182,7 @@ class Parser {
     this.addDropRate(result, data.drops);
     this.addPatchlogs(result, data.patchlogs);
     this.addAdditionalWikiaData(result, category, data.wikia);
+    this.detectPrime(result);
     this.addVaultData(result, data.vaultData);
     this.addResistanceData(result, category);
     this.applyOverrides(result);
@@ -757,6 +758,17 @@ class Parser {
 
     const logs = patchlogs.patchlogs.getItemChanges(target);
     if (logs.length) item.patchlogs = logs;
+  }
+
+  detectPrime(item) {
+    if (
+      !['Primary', 'Secondary', 'Warframes', 'Sentinels', 'Mods', 'Archwing', 'Arch-Melee', 'Arch-Gun'].includes(
+        item.category
+      )
+    )
+      return;
+
+    item.isPrime = item.uniqueName.includes('Prime');
   }
 
   /**
