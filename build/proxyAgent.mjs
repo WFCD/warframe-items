84e59b726e661c94996a16f769c6f5baa7063fe5
alecamaracm <alecamar@hotmail.es>
2023-03-16 23:00:27 +0100

    Logging about proxy configuration

--- build/proxyAgent.mjs
+++ build/proxyAgent.mjs
@@ -5,6 +5,7 @@ export default () => {
   switch (process.env.PROXY_TYPE) {
     case 'SOCKS5':
       if (process.env.PROXY_SOCKS5_HOST) {
+        console.log('SOCKS5 proxy will be used.');
         return new SOCKS5Agent({
           socksHost: process.env.PROXY_SOCKS5_HOST,
           socksPort: process.env.PROXY_SOCKS5_PORT,
@@ -12,13 +13,20 @@ export default () => {
           socksPassword: process.env.PROXY_SOCKS5_PASS,
         });
       }
+      console.warn('SOCKS5 proxy is selected but no configuration is provided.');
       return undefined;
+
     case 'HTTPS':
       if (process.env.PROXY_HTTPS_STRING) {
+        console.log('HTTPS proxy will be used.');
         return new HttpsProxyAgent(process.env.PROXY_HTTPS_STRING);
       }
+      console.warn('HTTPS proxy is selected but no configuration is provided.');
       return undefined;
     default:
+      console.log(
+        'No proxy will be used. This is most likely fine in developement but origin.warframe.com is usually blocked in most servers.'
+      );
       return undefined;
   }
 };
