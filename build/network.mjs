1388517626977aa1547e7d5738628350508a7ddc
alecamaracm <alecamar@hotmail.es>
2023-03-16 21:04:05 +0100

    Missing async and linting

--- build/network.mjs
+++ build/network.mjs
@@ -1,5 +1,5 @@
-import getProxyAgent from './proxyAgent.mjs';
 import fetch from 'node-fetch';
+import getProxyAgent from './proxyAgent.mjs';
 
 const prod = process.env.NODE_ENV === 'production';
 
@@ -8,13 +8,13 @@ const agent = getProxyAgent();
 // eslint-disable-next-line no-control-regex
 const sanitize = (str) => str.replace(/\\r|\r?\n|\x09/g, '').replace(/\\\\"/g, "'");
 
-export const get = (url, disableProxy = !prod, compress = false) => {
-    const res = await fetch(url, {
-        agent: disableProxy ? undefined : agent,
-    });
-    return compress === false ? Uint8Array.from(await res.buffer()) : res.text();
+export const get = async (url, disableProxy = !prod, compress = false) => {
+  const res = await fetch(url, {
+    agent: disableProxy ? undefined : agent,
+  });
+  return compress === false ? Uint8Array.from(await res.buffer()) : res.text();
 };
 
 export const getJSON = async (url, disableProxy) => {
-    return JSON.parse(sanitize(await get(url, disableProxy, true)));
+  return JSON.parse(sanitize(await get(url, disableProxy, true)));
 };
