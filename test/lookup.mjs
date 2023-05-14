e1886808aa1d3d2c343fd38d66025f3f6890a37f
Matej Voboril <tobiah@pm.me>
2023-05-14 12:17:09 -0500

    manually override resources

--- test/lookup.mjs
+++ test/lookup.mjs
@@ -1,3 +1,43 @@
 import Items from '../index.mjs';
+
 const items = new Items();
-console.error(items.find(i => i.name === 'Alloy Plate').type);
+
+const toFind = [
+  'Nano Spores',
+  'Alloy Plate',
+  'Salvage',
+  'Rubedo',
+  'Plastids',
+  'Circuits',
+  'Orokin Cell',
+  'Argon Crystal',
+  'Control Module',
+  'Mutalist Alad V Nav Coordinate',
+  'Cryptographic ALU',
+  'Omega Isotope',
+  'Riven Sliver',
+  'Kuva',
+  'Steel Essence',
+  'Morphics',
+  'Cryotic',
+  'Hexenon',
+  'Neural Sensors',
+  'Oxium',
+  'Polymer Bundle',
+  'Tellurium',
+  'Spectral Debris',
+  'Aya',
+  'Somatic Fibers',
+  'Endo',
+  'Nav Coordinate',
+  'Judgement Points',
+  'Synthula',
+  'Javlok Capacitor',
+  'Vitus Essence',
+  'Void Traces',
+  'Gallium',
+  'Antiserum Injector Fragment',
+];
+
+// eslint-disable-next-line no-console
+console.error(items.filter((i) => toFind.includes(i.name)).map((i) => `${i.uniqueName}:${i.type}`));
