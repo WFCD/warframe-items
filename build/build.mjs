e88d0a427a673ca2458e7bb2f91da2ad857d07f9
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2024-10-19 21:05:31 -0400

    perf: use the partial hash from textureLocation for caching

--- build/build.mjs
+++ build/build.mjs
@@ -225,10 +225,11 @@ class Build {
     const imageBase = manifest.find((i) => i.uniqueName === item.uniqueName);
     if (!imageBase) return;
     const imageStub = imageBase.textureLocation.replace(/\\/g, '/').replace('xport/', '');
+    const imageHash = imageStub.match(/!00_([\w\d\S]+)/);
     const imageUrl = `https://content.warframe.com/PublicExport/${imageStub}`;
     const basePath = fileURLToPath(new URL('../data/img/', import.meta.url));
     const filePath = path.join(basePath, item.imageName);
-    const hash = manifest.find((i) => i.uniqueName === item.uniqueName).fileTime;
+    const hash = manifest.find((i) => i.uniqueName === item.uniqueName).fileTime ?? imageHash[1];
     const cached = imageCache.find((c) => c.uniqueName === item.uniqueName);
 
     // We'll use a custom blueprint image
@@ -254,11 +255,8 @@ class Build {
           throw err;
         };
         const image = await get(imageUrl).catch(retry).catch(retry);
-        const hashImg = createHash('SHA256').update(image).digest('hex');
 
-        if (cached && cached.hash === hashImg) return;
-
-        this.updateCache(item, cached, hash ?? hashImg, isComponent);
+        this.updateCache(item, cached, hash, isComponent);
 
         await sharp(image).toFile(filePath);
         await minify([filePath], {
