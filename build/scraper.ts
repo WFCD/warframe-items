30efb961306572c3bac1123f738ccb5d6f2026db
Snor <snorlax150@gmail.com>
2026-09-08 01:17:12 -0500

    fix: convert in-code wikiaName override to wikiaNameOverrides.json

--- build/scraper.ts
+++ build/scraper.ts
@@ -21,8 +21,30 @@ import type { TitaniaRelic } from '@wfcd/relics';
 import type { Patchlogs } from '@wfcd/patchlogs';
 
 const locales = await readJson<string[]>(new URL('../config/locales.json', import.meta.url));
+const wikiaNameOverrides = await readJson<WikiaNameOverride[]>(new URL('../config/wikiaNameOverrides.json', import.meta.url));
 
 const prod = process.env.NODE_ENV === 'production';
+
+interface WikiaNameOverride {
+  name: string;
+  override: string;
+}
+
+/**
+ * Check for and apply overrides for WikiaData names
+ * @param wikiaData - set of WikiaData to check
+ */
+const applyNameOverrides = (wikiaData: WikiaData) => {
+  const overrideMap: Record<string, string> = Object.fromEntries(
+    wikiaNameOverrides.map((override) => [override.name, override.override])
+  );
+  for (const key of Object.keys(wikiaData) as (keyof WikiaData)[]) {
+    for (const entry of wikiaData[key]) {
+      const replacement = overrideMap[entry.name];
+      if (replacement) entry.name = replacement;
+    }
+  }
+};
 
 interface ApiChunk {
   category: string;
@@ -313,7 +335,7 @@ class Scraper {
     const vaultData = await new VaultScraper().scrape();
     bar.tick();
 
-    return {
+    const wikiaToReturn: WikiaData = {
       weapons,
       warframes,
       necramechs,
@@ -325,6 +347,9 @@ class Scraper {
       arcanes,
       vaultData,
     };
+
+    applyNameOverrides(wikiaToReturn);
+    return wikiaToReturn;
   }
 
   /**
